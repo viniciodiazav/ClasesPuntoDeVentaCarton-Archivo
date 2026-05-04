297036c673d8eddb180c5a9fb0b5768347f74a3e
Leonardo Vinicio Diaz Avelar <ldiaza2200@alumno.ipn.mx>
2026-05-03 23:24:55 -0600

    .

--- CompraMayoreo.java
+++ CompraMayoreo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
@@ -34,7 +35,7 @@ public class CompraMayoreo {
     private long pesoNeto;
     
     @ManyToOne
-    @Column(name = "materal", nullable = false)
+    @JoinColumn(name = "id_material")
     private Material material;
     
 }
